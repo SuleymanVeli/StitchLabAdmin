73777d0654ee4bd93f513a828b19d82e14bed61d
Suleyman <veli.suleyman@gmail.com>
2026-04-07 21:15:54 +0400

    feat: Fix product creation timestamp assignment to use current date

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -57,7 +57,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       product.markModified('sections');
       product.markModified('preparation');
       product.markModified('abbreviations');
-      product.createdAt = Date.now;
+      product.createdAt = new Date()
       // 4. Yadda saxla
       await product.save();
 
